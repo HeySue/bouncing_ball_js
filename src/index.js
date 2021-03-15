e1bf9468a62555f251264494b3bdf51b94adf74d
Yiping  Su <syp199701@gmail.com>
2021-03-15 14:17:11 +1100

    paddle can move and stop at the boundaries

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-let paddle = new Paddle();
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
+
+function gameLoop(){
+    ctx.clearRect(0,0, GAME_WIDTH, GAME_HEIGHT);
+    paddle.update(ctx); 
+    paddle.draw(ctx);
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
 console.log("index");
--- src/paddle.js
+++ src/paddle.js
@@ -1,16 +1,35 @@
 export default class Paddle{
-    constructor(){
-        this.width = 60;
-        this.height = 12;
-        console.log("h");
+    constructor(gameWidth, gameHeight, maxSpeed=4){
+        this.width = 110;
+        this.height = 15;
+
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.position = {
+            x: (gameWidth - this.width) /2,
+            y: gameHeight - this.height - 10
+        }
+        this.maxSpeed = maxSpeed;
+        this.speed = -this.maxSpeed;
     }
 
     draw(ctx){
         ctx.fillStyle = "#ee8c86";
-        ctx.fillRect(200, 200, this.width, this.height);
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
         // console.log('draw');
     }
 
+    update(ctx){
+        this.position.x += this.speed;
+
+        if (this.position.x < 0 ) 
+            this.position.x = 0;
+        if (this.position.x > this.gameWidth - this.width) {
+            this.position.x = this.gameWidth - this.width;
+            // this.speed = 0;
+        }
+        // console.log(this.position.x);
+    }
 
 }
     
